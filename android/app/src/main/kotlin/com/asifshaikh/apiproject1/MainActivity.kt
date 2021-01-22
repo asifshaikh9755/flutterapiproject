b460c64a9482dd958b8ee94a06878fcb502121b0
asifshaikh <asif@example.com>
2021-01-22 11:10:10 +0000

    showing error type 'list' is not subtype of type 'string' of 'other'

---
+++ android/app/src/main/kotlin/com/asifshaikh/apiproject1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.asifshaikh.apiproject1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/HomePage.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HomePage extends StatefulWidget {
+  final String title;
+
+  HomePage({Key key, @required this.title}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final String url = "https://randomuser.me/api/?results=50";
+  List userData;
+  bool _loading = true;
+
+  Future getData() async {
+    var response = await http.get(Uri.encodeFull(url),headers: {"Accept":"application/json"});
+
+    List data = jsonDecode(response.body)['results'];
+    print(data);
+    setState(() {
+      userData = data;
+      _loading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(
+      child: _loading
+          ? CircularProgressIndicator()
+          : ListView.builder(
+              itemCount: userData == null ? 0 : userData.length,
+              itemBuilder: (context, index) {
+                return Card(
+                    child: Row(
+                  children: <Widget>[
+                    Container(
+                        margin: EdgeInsets.all(20),
+                        child: Image(
+                          width: 70,
+                          height: 70,
+                          fit: BoxFit.contain,
+                          image: NetworkImage(
+                              userData[index]['picture']['thumbnail']),
+                        )),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            userData[index]['name']['title']+['first'] +
+                                userData[index]['name']['last'],
+                            style: TextStyle(fontSize: 20, color: Colors.black),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ));
+              }),
+    )
+    );
+  }
+}
